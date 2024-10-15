028d9e7bd1a368dfbbdc17e76ce9d03966519217
Chang ShengWen <asdzxc1234448@gmail.com>
2024-10-15 16:08:39 +0800

    online test 09

--- src/hooks/useGetLoginInfo.js
+++ src/hooks/useGetLoginInfo.js
@@ -1,7 +1,7 @@
 import { useGetUserQuery } from "../store";
 
 export default function useGetLoginInfo() {
-  const { data } = useGetUserQuery();
+  const data = useGetUserQuery();
   console.log(data);
   return data;
 }
